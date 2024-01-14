1155575076bd62af5bc4c759a8c1700d23fb408b
Shafath Ahmed <shafath.001@gmail.com>
2024-01-14 11:23:23 +0900

    New feature 2 change

--- src/main/java/com/abc/productservice/controller/ProductController.java
+++ src/main/java/com/abc/productservice/controller/ProductController.java
@@ -17,7 +17,7 @@ public class ProductController {
 
     @GetMapping("")
     public Flux<ProductDto> getAllProducts()  {
-        System.out.println("New Feature 2");
+        System.out.println("New Feature 2 changes to track");
         return this.productService.getAll();
     }
 

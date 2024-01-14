c3c7d361e0ca9c306dbe1bb21651774c6de014f2
Shafath Ahmed <shafath.001@gmail.com>
2024-01-14 11:46:21 +0900

    New feature 2 change ghi

--- src/main/java/com/abc/productservice/controller/ProductController.java
+++ src/main/java/com/abc/productservice/controller/ProductController.java
@@ -17,7 +17,7 @@ public class ProductController {
 
     @GetMapping("")
     public Flux<ProductDto> getAllProducts()  {
-        System.out.println("New Feature 2 changes to track 3 for nothing");
+        System.out.println("New Feature 2 changes to track 3 for nothing 3");
         return this.productService.getAll();
     }
 
